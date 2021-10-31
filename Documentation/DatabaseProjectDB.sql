39ebdc6df5f4b7f962525f3007a7503421ce4152
NicolaDean <nicola.dean@mail.polimi.it>
2021-10-31 22:56:00 +0100

    Changed DB dump (modified services tables)

--- Documentation/DatabaseProjectDB.sql
+++ Documentation/DatabaseProjectDB.sql
@@ -1,11 +1,26 @@
 
+drop table if exists Users;
+
+create table Users(
+                      id 			INT AUTO_INCREMENT,
+                      username    VARCHAR(30) not null,
+                      password	VARCHAR(30) not null,
+                      email		VARCHAR(30) not null,
+                      PRIMARY KEY(id)
+);
+
+insert into Users (username,password,email) values ("nico","1234","nico@gmail.com");
+insert into Users (username,password,email) values ("fasa","1234","fasa@gmail.com");
+insert into Users (username,password,email) values ("babbano","1234","babbano@gmail.com");
+
+-- Now create the optionals products
 drop table if exists OptionalProducts;
 
 create table OptionalProducts(
-	id 			INT AUTO_INCREMENT,
-    name 		VARCHAR(30) NOT NULL,
-    monthlyFee  INT NOT NULL,
-    PRIMARY KEY(id)
+                                 id 			INT AUTO_INCREMENT,
+                                 name 		VARCHAR(30) NOT NULL,
+                                 monthlyFee  INT NOT NULL,
+                                 PRIMARY KEY(id)
 );
 
 insert into OptionalProducts (name,monthlyFee) values ("Iphone 8",20);
@@ -23,9 +38,9 @@ drop table if exists Rate_costs;
 drop table if exists Packages;
 
 create table Packages(
-	id 				INT AUTO_INCREMENT,
-    name			VARCHAR(30) not null,		
-    PRIMARY KEY(id)
+                         id 				INT AUTO_INCREMENT,
+                         name			VARCHAR(30) not null,
+                         PRIMARY KEY(id)
 );
 
 insert into Packages (name) values ("Basic");
@@ -40,12 +55,12 @@ select * from Packages;
 
 
 create table Rate_costs(
-	id 				INT AUTO_INCREMENT,
-    monthValidity   INT not null,
-    cost			INT not null,
-    packageId       INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY(packageId) REFERENCES Packages(id)
+                           id 				INT AUTO_INCREMENT,
+                           monthValidity   INT not null,
+                           cost			INT not null,
+                           packageId       INT,
+                           PRIMARY KEY(id),
+                           FOREIGN KEY(packageId) REFERENCES Packages(id)
 );
 
 insert into Rate_costs (monthValidity,cost,packageId) values (12,20,1);
@@ -66,41 +81,43 @@ drop table if exists fixed_internet_services;
 drop table if exists services;
 
 create table services(
-	id				INT AUTO_INCREMENT,
-    PRIMARY KEY(id)
+                         id				INT AUTO_INCREMENT,
+                         packageId       INT not null,
+                         DTYPE	        VARCHAR(3),
+                         PRIMARY KEY(id),
+                         FOREIGN KEY(packageId) REFERENCES Packages (id)
 );
 
 create table mobile_phone_services(
-	id 				INT AUTO_INCREMENT,
-    minutes         INT not null,
-    sms				INT not null,
-    extraMinutesFee FLOAT not null,
-    extraSMSFee		FLOAT not null,
-    serviceId		INT not null,
-    PRIMARY KEY(id),
-    FOREIGN KEY (serviceId) REFERENCES services (id)
+                                      id 				INT,
+                                      minutes         INT not null,
+                                      sms				INT not null,
+                                      extraMinutesFee FLOAT not null,
+                                      extraSMSFee		FLOAT not null,
+                                      PRIMARY KEY(id),
+                                      FOREIGN KEY (id) REFERENCES services (id)
 );
 
 -- and for fixed_internet???? is the same table, do 2 table or keep them united?
 create table mobile_internet_services(
-	id 				INT AUTO_INCREMENT,
-    gigabyte		INT not null,
-    extraFee        FLOAT not null,
-    serviceId		INT not null,
-    PRIMARY KEY(id),
-    FOREIGN KEY (serviceId) REFERENCES services (id)
+                                         id 				INT AUTO_INCREMENT,
+                                         gigabyte		INT not null,
+                                         extraFee        FLOAT not null,
+                                         PRIMARY KEY(id),
+                                         FOREIGN KEY (id) REFERENCES services (id)
 );
 
 
-insert into services () values ();-- 1
-insert into services () values ();-- 2
-insert into services () values ();-- 3
-insert into services () values ();-- 4
-insert into services () values ();-- 5
+insert into services (packageID,DTYPE) values (1,"MIS");-- 1
+insert into services (packageID,DTYPE) values (2,"MIS");-- 2
+insert into services (packageID,DTYPE) values (2,"MPS");-- 3
+insert into services (packageID,DTYPE) values (3,"MPS");-- 4
 
-insert into mobile_internet_services (gigabyte,extraFee,serviceId) values (5,0.5,1);
-insert into mobile_internet_services (gigabyte,extraFee,serviceId) values (10,0.3,2);
+insert into mobile_internet_services (id,gigabyte,extraFee) values (1,5,0.5);
+insert into mobile_internet_services (id,gigabyte,extraFee) values (2,10,0.3);
 
-insert into mobile_phone_services (minutes,sms,extraMinutesFee,extraSMSFee,serviceId) values (500,100,0.1,0.1,3);
-insert into mobile_phone_services (minutes,sms,extraMinutesFee,extraSMSFee,serviceId) values (1000,300,0.07,0.1,4);
+insert into mobile_phone_services (id,minutes,sms,extraMinutesFee,extraSMSFee) values (3,500,100,0.1,0.1);
+insert into mobile_phone_services (id,minutes,sms,extraMinutesFee,extraSMSFee) values (4,1000,300,0.07,0.1);
 
+select * from services;
+select * from services natural join mobile_phone_services;
