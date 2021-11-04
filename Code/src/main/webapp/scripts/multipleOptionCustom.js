2771c9b2c6b87bc0e5f98f02ad60d04ee818b323
NicolaDean <nicola.dean@mail.polimi.it>
2021-11-04 22:19:40 +0100

    Improved multipleOptionCustom with css selection system

--- Code/src/main/webapp/scripts/multipleOptionCustom.js
+++ Code/src/main/webapp/scripts/multipleOptionCustom.js
@@ -1,10 +1,16 @@
 
 //Modify tag of select
-function multiple_custom(customTag,id)
+function multiple_custom(caller,customTag,id)
 {
     var option = document.getElementById(customTag+"-"+id);
-
     option.selected = !option.selected;
+    if(option.selected)
+    {
+        caller.className = "card text-white bg-secondary mb-3";
+    }
+    else{
+        caller.className = "card border-secondary mb-3";
+    }
 }
 /*
 function multipleOptionCustom(selectorId,elemetsTag)
