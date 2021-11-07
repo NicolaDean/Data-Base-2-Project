eb2acc6b195435c8c5bf26733fbefd12b235f35c
Marco Fasanella <marco.fasanella@mail.polimi.it>
2021-11-07 16:37:01 +0100

    sql

--- DatabaseProjectDB.sql
+++ DatabaseProjectDB.sql
@@ -13,13 +13,15 @@ create table Users(
                       username    VARCHAR(30) not null,
                       password	VARCHAR(30) not null,
                       email		VARCHAR(30) not null,
-
+                      type      varchar(30) not null,
                       PRIMARY KEY(id)
 );
 
-insert into Users (username,password,email) values ("nico","1234","nico@gmail.com");
-insert into Users (username,password,email) values ("fasa","1234","fasa@gmail.com");
-insert into Users (username,password,email) values ("babbonatale","1234","babbano@gmail.com");
+insert into Users (username,password,email,type) values ("nico","1234","nico@gmail.com","user");
+insert into Users (username,password,email,type) values ("fasa","1234","fasa@gmail.com","user");
+insert into Users (username,password,email,type) values ("babbano","1234","babbano@gmail.com","user");
+insert into Users (username,password,email,type) values ("admin","1234","fasa@gmail.com","admin");
+
 select * from Users;
 -- Now create the optionals products
 drop table if exists OptionalProducts;
