2156b67264230cd10049b850ff927eb31a48aba2
Marco Fasanella <marco.fasanella@mail.polimi.it>
2021-11-07 16:36:12 +0100

    header.html fixed Log out

--- Code/src/main/java/it/polimi/db2/controllers/CheckLogin.java
+++ Code/src/main/java/it/polimi/db2/controllers/CheckLogin.java
@@ -61,6 +61,15 @@ public class CheckLogin extends BasicServerlet {
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session=request.getSession();
+        session.invalidate();
+        this.setError(request,response,"You are successfully logged out!", TemplatePathManager.loginPage);
+
+    }
+
     public void destroy() {
     }
 }
