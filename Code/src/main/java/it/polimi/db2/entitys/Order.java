f2ef645128cbb8fa87082e762307d4ab443d05ce
NicolaDean <nicola.dean@mail.polimi.it>
2021-11-07 23:40:21 +0100

    Now When i click solvePayment i can remove tag "failed" from order (//TODO trigger that remove insolvance from user)

--- Code/src/main/java/it/polimi/db2/entitys/Order.java
+++ Code/src/main/java/it/polimi/db2/entitys/Order.java
@@ -9,6 +9,7 @@ import java.util.List;
 @NamedQuery(name="Orders.Id"             ,  query="select o from Order o where o.Id = :orderId")
 @NamedQuery(name="Orders.All"             , query="select o from Order o") //TODO adjust query for only accepted orders
 @NamedQuery(name="Orders.Suspended"       , query="select o from Order o where o.status=false")
+@NamedQuery(name="Orders.RemoveSuspend"   , query="update Order o set o.status = true where o.Id=:orderId")
 @NamedQuery(name="PurchasesByPackages"    , query = "select count (distinct o) from Order o group by o.pack")
 @NamedQuery(name="Orders.UserInsolvances" , query = "select o from Order o where o.status=false and o.user.id = :userId")
 //TODO think a new entity for statistical pourpose should be created to store pair "package-> quantity"
@@ -21,7 +22,7 @@ public class Order {
     Date startDate;
     Date creationDate;
     float totalPayment;
-    boolean status;
+    Boolean status =null;
 
     @OneToOne
     @JoinColumn(name = "userId")
@@ -115,5 +116,9 @@ public class Order {
 
     public int getId() {
         return Id;
+    }
+
+    public boolean toUpdate() {
+        return this.status != null;
     }
 }
