8a232540f2907fd88c59ca5c6dae3dc282bed914
Marco Fasanella <marco.fasanella@mail.polimi.it>
2021-12-17 13:07:24 +0100

    profile

--- Code/src/main/java/it/polimi/db2/controllers/Profile.java
+++ Code/src/main/java/it/polimi/db2/controllers/Profile.java
@@ -1,8 +1,11 @@
 package it.polimi.db2.controllers;
 
 import it.polimi.db2.entitys.User;
+import it.polimi.db2.exception.ElementNotFound;
+import it.polimi.db2.services.UserServices;
 import it.polimi.db2.utils.TemplatePathManager;
 
+import javax.ejb.EJB;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,11 +14,22 @@ import java.io.IOException;
 
 @WebServlet(name = "profile", value = "/profile")
 
+
 public class Profile extends BasicServerlet{
+    @EJB(name="it.polimi.db2.services/UserService")
+    private UserServices userServices;
+
+
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         HttpSession session = request.getSession();
         User user  = (User) session.getAttribute("user") ;
+        try {
+            user=userServices.getUserById(user.getId());
+            session.setAttribute("user",user);
+        } catch (ElementNotFound elementNotFound) {
+            elementNotFound.printStackTrace();
+        }
         request.setAttribute("user", user);
         request.setAttribute("register", TemplatePathManager.registration);
         request.setAttribute("log in", TemplatePathManager.loginPage);
